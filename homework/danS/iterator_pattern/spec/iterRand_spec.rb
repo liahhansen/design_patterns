1ffeec993f0cde4eb989bf30365bf69373e2b142
Dan Steinicke <dansteinicke@gmail.com>
2010-06-04 09:27:14 -0700

    Add a spec to test distribution of returned random values.  All possible values should recur the same number of times, plus or minus 30%.  Currently fails.

--- homework/danS/iterator_pattern/spec/iterRand_spec.rb
+++ homework/danS/iterator_pattern/spec/iterRand_spec.rb
@@ -23,12 +23,49 @@ describe "C class" do
         @values.each {|val| @w.next.should == val}
       end
       it "should return random elements from the set of elements returned by @iter.next" do
-        stub!(:rand).and_return(1,2,4,50)
+        # stub!(:rand).and_return(1,2,4,50)
         (0..9).each do |i|  
           nextVal = @w.next
           randVal = @w.random
           @values[0..i].should include(randVal)
-          puts "for a next value of #{nextVal} the random value is #{randVal}"
+          #puts "for a next value of #{nextVal} the random value is #{randVal}"
+        end
+      end
+      it "should distribute random values evenly within 30%" do
+        #setup
+        results = Hash.new(0)
+        repeat = 10000
+        number_of_values = 10
+        
+        #collect values rand could return as keys
+        number_of_values.times {results[@w.next] = 0}
+        
+        #call random multiple times and count occurrances of each possible value
+        repeat.times do
+          randValue = @w.random
+          results[randValue] = results[randValue] + 1
+        end
+        
+        #calculate range plus and minus 30% of equal
+        allowable_difference = ((repeat / number_of_values) * 0.3).to_i
+        highest_count = ((repeat / number_of_values) + allowable_difference).to_i
+        lowest_count = ((repeat / number_of_values) - allowable_difference).to_i
+        allowable_range = (lowest_count..highest_count)
+        
+        #print allowable range to screen
+        puts "calling @w.random #{repeat} times with #{number_of_values} possible values"
+        puts "each value should recur between #{lowest_count} and #{highest_count} times"
+        
+        
+        #print results for developer
+        results.keys.sort.each do |key|
+          pass = allowable_range.include?(results[key]) ? 'PASS' : 'FAIL'
+          puts "value #{key.to_s.rjust(3)} occurring #{results[key].to_s.rjust(5)} times is in allowable range #{pass}"
+        end
+        
+        #test that results spread equally within allowable range
+        results.keys.sort.each do |key|
+          allowable_range.should include(results[key]) 
         end
       end
     end
